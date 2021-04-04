da66da2022a500e0c0e4d019e2bb90e88266c4a6
Keri Carpenter <kericarpenter@gmail.com>
2021-04-04 14:42:04 -0700

    fetch api work

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
-console.log('Hello World!');
-fetch('https://reqres.in/api/users/6')
-    .then(res => {
-        if (res.ok) {
-            console.log('SUCCESS')
-        } else {
-            console.log('Not Successful')
-        }
-    }
-        )
+// This exercise is from Web Dev Simplified and this exercise:
+// https://youtu.be/cuEtnrL9-H0
+
+// Just testing that the index.js file is being read properly
+// console.log('Hello World!');
+
+fetch('https://reqres.in/api/users')
+    .then(res => res.json())
     .then(data => console.log(data))
