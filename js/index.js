f58a8b0ff13c9fb8ac3b9368d8b698c9fb6cc631
Keri Carpenter <kericarpenter@gmail.com>
2021-04-04 16:01:35 -0700

    index.html

--- js/index.js
+++ js/index.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-console.log('Hello World!');
-fetch('https://reqres.in/api/users/6')
-    .then(res => {
-        if (res.ok) {
-            console.log('SUCCESS')
-        } else {
-            console.log('Not Successful')
-        }
-    }
-        )
-=======
 // This exercise is from Web Dev Simplified and this exercise:
 // https://youtu.be/cuEtnrL9-H0
 
@@ -18,5 +6,4 @@ console.log('This printed from index.js!');
 
 fetch('https://reqres.in/api/users')
     .then(res => res.json())
->>>>>>> 23187f94ba239ad56f55ff1ab8ab880413e84c37
     .then(data => console.log(data))
