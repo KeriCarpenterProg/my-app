23187f94ba239ad56f55ff1ab8ab880413e84c37
Keri Carpenter <56651871+KeriCarpenterProg@users.noreply.github.com>
2021-04-04 15:52:29 -0700

    Pullrequest2 (#11)
    * javascript and api playing
    
    * fetch api work
    
    * small change
    
    * very small change

--- js/index.js
+++ js/index.js
@@ -2,16 +2,8 @@
 // https://youtu.be/cuEtnrL9-H0
 
 // Just testing that the index.js file is being read properly
-// console.log('Hello World!');
+console.log('This printed from index.js!');
 
 fetch('https://reqres.in/api/users')
-    .then(res => {
-        if (res.ok) {
-            console.log('SUCCESS')
-        } else {
-            console.log('Not Successful')
-        }
-    }
-        )
     .then(res => res.json())
     .then(data => console.log(data))
