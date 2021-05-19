60b2efedcff6190a88b89c2ad280fc046984b7da
Keri Carpenter <kericarpenter@gmail.com>
2021-05-19 07:02:03 -0700

    palindrome check

--- js/index.js
+++ js/index.js
@@ -2,11 +2,7 @@
 // https://youtu.be/cuEtnrL9-H0
 
 // Just testing that the index.js file is being read properly
-<<<<<<< HEAD
 console.log('This printed from index.js!');
-=======
-// console.log('Hello World!');
->>>>>>> pullrequest1
 
 fetch('https://reqres.in/api/users')
     .then(res => res.json())
