d4f5500af6df7e4c35dfc631987cabc3c4d23eb7
Keri Carpenter <keri.carpenter@sony.com>
2021-06-11 21:53:17 -0400

    another day of work

--- js/javascriptcss.js
+++ js/javascriptcss.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('#mobile-menu');
 
 const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
 
 // Display Mobile Menu
 const mobileMenu = () => {
@@ -9,3 +10,14 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+
+//  Show active menu when scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const elem = document.querySelector('#home-page');
+    const elem = document.querySelector('.highlight');
+    const elem = document.querySelector('.highlight');
+    const elem = document.querySelector('.highlight');
+
+}
