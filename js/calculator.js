c939147397ac55ca4536bf6ff84a5038b1ab9be7
Keri Carpenter <keri.carpenter@sony.com>
2021-06-13 15:03:25 -0400

    showing tim something

---
+++ js/calculator.js
@@ -0,0 +1,4 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation');
+console.log(numberButtons);
+console.log(operationButtons);
