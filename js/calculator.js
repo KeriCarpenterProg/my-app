1e718c3c47d6d89b82fe9fede3c5f517b23b5d9d
Keri Carpenter <56651871+KeriCarpenterProg@users.noreply.github.com>
2021-06-20 12:38:02 -0700

    calculator (#34)
    Co-authored-by: Keri Carpenter <keri.carpenter@sony.com>

--- js/calculator.js
+++ js/calculator.js
@@ -1,4 +1,152 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    clear() {   
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+        this.updateDisplay();
+    }
+
+    delete() {
+        console.log("Before slicing -1 "+ this.currentOperand);
+        this.currentOperand = this.currentOperand.toString().slice(0,-1);
+        console.log("After sliceing -1 "+this.currentOperand);
+    }
+
+    appendNumber(number) {
+        if(number === '.' && this.currentOperand.includes('.')) return;
+        if(isNaN(this.currentOperand)) 
+        { 
+                this.currentOperand = number.toString();
+        } else this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+
+    compute() {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        // console.log("Previous Number is "+ this.previousOperand);
+        // console.log("Current Operand is "+ this.currentOperand);
+        const curr = parseFloat(this.currentOperand);
+        if(isNaN(prev) || isNaN(curr)) return;
+        switch(this.operation) {
+            case '+':
+                computation = prev + curr;
+                break;
+            case '-':
+                computation = prev - curr;
+                break;
+            case '*':
+                computation = prev * curr;
+                break;
+            case '%':
+                computation = prev / curr;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+        // console.log("The value of computation is " + computation);
+    }
+
+    getDisplayNumber(number){
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.'));
+        const decimalDigits = stringNumber.split('.')[1];
+        let integerDisplay
+        if(isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {
+             maximumFractionDigits: 0   
+            })
+        }
+        if (decimalDigits != null) {
+            return `${integereDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay;
+        }
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = 
+        this.getDisplayNumber(this.currentOperand);
+        if(this.operation != null){
+            this.previousOperandTextElement.innerText = 
+            `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+        } else {
+           this.previousOperandTextElement.innerText = ''; 
+        }
+    }
+}
+
+
+// numberButtons is an array with the objects of all
+// the numbers on the calculator 1,2,3 including the '.'
 const numberButtons = document.querySelectorAll('[data-number]');
 const operationButtons = document.querySelectorAll('[data-operation');
-console.log(numberButtons);
-console.log(operationButtons);
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+
+
+const calculator = new Calculator(previousOperandTextElement,
+    currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay();
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText);
+        calculator.updateDisplay();
+    })
+})
+
+equalsButton.addEventListener('click', () => {
+    calculator.compute();
+    calculator.updateDisplay();
+    // STOPPED HERE!  How do I clear this?????? This is not working yet.  I just added this but didn't test it.
+ 
+    this.currentOperand = '';
+
+    this.previousOperand = '';
+    this.operation = undefined;
+    console.log("Previous Number is "+ this.previousOperand);
+    console.log("Current Number is "+ this.currentOperand);
+    console.log("The saved operation is " + this.operation);
+})
+
+allClearButton.addEventListener('click', () => {
+    calculator.clear();
+    calculator.updateDisplay();
+})
+
+deleteButton.addEventListener('click', () => {
+    calculator.delete();
+    calculator.updateDisplay();
+})
