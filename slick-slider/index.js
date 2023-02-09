9ba75a69059bfdb266ce0a2bc87676412d52a980
Keri Carpenter <56651871+KeriCarpenterProg@users.noreply.github.com>
2023-02-08 20:25:14 -0800

    not sure (#56)

---
+++ slick-slider/index.js
@@ -0,0 +1,11 @@
+// Basic initialization is like this:
+// $('.your-class').slick();
+
+// I added some other properties to customize my slider
+// Play around with the numbers and stuff to see
+// how it works.
+$(".slick-carousel").slick({
+  slidesToShow: 3,
+  dots: true,
+  centerMode: true,
+});
